ad205e46a268dbdf9b9513f276ca7bcfed128a50
User <ss.bykoff@gmail.com>
2022-11-21 14:05:55 +0300

    добавлен троттлинг

--- orders/settings.py
+++ orders/settings.py
@@ -168,6 +168,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
